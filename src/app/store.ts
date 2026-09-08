fc7e24cc67645258bcd622b01b72270958c9b92d
volha-codes <yankovich.volha@gmail.com>
2026-09-08 23:18:24 +0200

    feat: configure redux store

---
+++ src/app/store.ts
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import accountsReducer from "../features/accounts/accountsSlice";
+
+export const store = configureStore({
+  reducer: {
+    accounts: accountsReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
