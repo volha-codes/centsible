113b455e63e3bab899cd1a67c5795740d85da78c
volha-codes <yankovich.volha@gmail.com>
2026-09-08 22:38:07 +0200

    chore: bootstrap project

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="p-8">Centsible</div>
+}
+
+export default App
---
+++ src/types/index.ts
@@ -0,0 +1,38 @@
+export interface Account {
+  id: string
+  name: string
+  currency: string
+  startingBalance: number
+}
+
+export interface Category {
+  id: string
+  name: string
+  icon: string
+  color: string
+  type: 'income' | 'expense'
+}
+
+export interface Transaction {
+  id: string
+  accountId: string
+  categoryId: string
+  amount: number
+  date: number
+  note: string
+}
+
+interface RecurringRuleBase {
+  id: string
+  accountId: string
+  categoryId: string
+  amount: number
+  note: string
+  startDate: number
+  lastGeneratedDate?: number
+  endDate?: number
+}
+
+export type RecurringRule =
+  | (RecurringRuleBase & { frequency: 'monthly'; dayOfMonth: number })
+  | (RecurringRuleBase & { frequency: 'weekly'; dayOfWeek: number })
